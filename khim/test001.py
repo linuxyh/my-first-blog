4104f92b9e44e22142a36d5e7deff87f771294f4
ragond <yonux@yahoo.co.kr>
2017-04-16 22:44:59 +0900

    My Django Girls app, 첫번째 커밋

---
+++ khim/test001.py
@@ -0,0 +1,14 @@
+for treeHit in range(10):
+    print("%d" % treeHit)
+
+marks = [90,25,67,45,80]
+print(marks[0:3])
+
+
+
+#treeHit = 0
+#while treeHit < 10:
+#    treeHit += 1
+#    print("%d번 나무를 찍었어요" % treeHit)
+#    if treeHit == 10:
+#        print("나무")
